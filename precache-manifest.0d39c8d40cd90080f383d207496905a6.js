5011921500619fa191c68a642916528a7335a43f
Fernanda Marroquin <fernanda.marroquin07@gmail.com>
2019-04-01 19:10:32 -0400

    Updates

---
+++ precache-manifest.0d39c8d40cd90080f383d207496905a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "affd076930f72d32a23a76fa43e66975",
+    "url": "/Clicky-Game/static/media/yoshi.affd0769.jpg"
+  },
+  {
+    "revision": "e15e488f874384b3d0d00a9100bcbd19",
+    "url": "/Clicky-Game/static/media/peach.e15e488f.png"
+  },
+  {
+    "revision": "3dc23efc8a6b148ee3daf313e39fcd5b",
+    "url": "/Clicky-Game/static/media/mario.3dc23efc.png"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "e22513c190c0274cca89",
+    "url": "/Clicky-Game/static/js/main.1d2bbcff.chunk.js"
+  },
+  {
+    "revision": "bed643ea30c914e046c7",
+    "url": "/Clicky-Game/static/js/2.47eb558c.chunk.js"
+  },
+  {
+    "revision": "e22513c190c0274cca89",
+    "url": "/Clicky-Game/static/css/main.21b0d39b.chunk.css"
+  },
+  {
+    "revision": "f5b961bb58f99616961e65e378a43e9b",
+    "url": "/Clicky-Game/index.html"
+  }
+];
